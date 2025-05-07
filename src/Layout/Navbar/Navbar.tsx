c0c2186588b15facb28d4aba7c857322385ae715
Михаил Жирок <mihail.zhirok@gmail.com>
2025-05-08 00:05:07 +0300

    add mailer res3

--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -13,11 +13,12 @@ export const Navbar = () => {
 
     <div className='navbarWrap'>
   
-        <Link className='navbarLink left' to="/our-links">
-        Волонтерські організації 
-        </Link>
-        <Link className='navbarLink' to="/">
+       
+        <Link className='navbarLink left' to="/">
           Головна
+        </Link>
+        <Link className='navbarLink' to="/our-links">
+        Волонтерські організації 
         </Link>
         <Link className='navbarLink' to="/experience">
         Досвід людей
