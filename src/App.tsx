32d9b488b9e9d14d43ae44de47116ba183459e4e
Михаил Жирок <mihail.zhirok@gmail.com>
2025-05-08 14:00:31 +0300

    add mailer res3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
+import { useEffect } from "react";
+
 import { Main } from './pages/Main/Main';
 import { Layout } from './Layout/Layout';
 import { LinksList } from './pages/LinksList/LinksList';
@@ -9,11 +11,31 @@ import ExpPage from './pages/ExpPafe/ExpPage';
 import AddExperience from './pages/AddExperience/AddExperience';
 import DocumentsSection from './pages/DocumentsSection/DocumentsSection';
 
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const elementId = location.hash.slice(1);
+      const element = document.getElementById(elementId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+        return;
+      }
+    }
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location]);
+
+  return null;
+}
 function App() {
 
 
   return (
     <>
+    <ScrollToTop />
     <BrowserRouter>
 
     <Layout>
